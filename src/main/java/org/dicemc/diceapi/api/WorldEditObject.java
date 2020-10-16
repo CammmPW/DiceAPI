81e7d85591e87765b20e4eee030d7cce42c814ad
ChimpGamer <Tim.Cremers@Ziggo.nl>
2020-10-16 14:22:47 +0200

    Added the save clipboard and selection as schematic

--- src/main/java/org/dicemc/diceapi/api/WorldEditObject.java
+++ src/main/java/org/dicemc/diceapi/api/WorldEditObject.java
@@ -1,17 +1,31 @@
 package org.dicemc.diceapi.api;
 
+import com.boydti.fawe.Fawe;
+import com.boydti.fawe.object.clipboard.SimpleClipboard;
+import com.sk89q.worldedit.EmptyClipboardException;
 import com.sk89q.worldedit.LocalConfiguration;
 import com.sk89q.worldedit.LocalSession;
 import com.sk89q.worldedit.bukkit.BukkitWorld;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.command.SchematicCommands;
+import com.sk89q.worldedit.extension.platform.Actor;
+import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
+import com.sk89q.worldedit.extent.clipboard.Clipboard;
+import com.sk89q.worldedit.extent.clipboard.io.BuiltInClipboardFormat;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardWriter;
 import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.CuboidRegion;
+import com.sk89q.worldedit.regions.Region;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.dicemc.diceapi.DiceAPI;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class WorldEditObject {
@@ -58,5 +72,41 @@ public class WorldEditObject {
             return;
         }
         clipboardFormat.load(schematicFile).paste(localWorld, location, true, false, null);
+    }
+
+    public void pasteSchematic(String world, String schematic, Location location) {
+        try {
+            pasteSchematicAtVector(world, schematic, toVector(location));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void savePlayerClipboardAsSchematic(Player player, File file) {
+        LocalSession session = we.getSession(player);
+        try (ClipboardWriter writer = BuiltInClipboardFormat.SPONGE_SCHEMATIC.getWriter(new FileOutputStream((file)))) {
+            // For some reason clipboard is deprecated while it's still being used by the internals of FAWE.
+            writer.write(session.getClipboard().getClipboard());
+        } catch (EmptyClipboardException ex) {
+            // Notify the player about empty clipboard
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void savePlayerSelectionAsSchematic(Player player, File file) {
+        LocalSession session = we.getSession(player);
+        Actor actor = we.wrapPlayer(player);
+        Region region = session.getSelection(session.getSelectionWorld());
+        Clipboard clipboard = new BlockArrayClipboard(region, actor.getUniqueId());
+        clipboard.setOrigin(session.getPlacementPosition(actor));
+        try (ClipboardWriter writer = BuiltInClipboardFormat.SPONGE_SCHEMATIC.getWriter(new FileOutputStream((file)))) {
+            // For some reason clipboard is deprecated while it's still being used by the internals of FAWE.
+            writer.write(session.getClipboard().getClipboard());
+        } catch (EmptyClipboardException ex) {
+            // Notify the player about empty clipboard
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
