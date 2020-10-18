0ea7c85ff12daff1a7499479f426105dcffbb95d
Cam <cam@cammm.pw>
2020-10-18 11:46:40 +0100

    Attempt one at making Schematic Pasting work.

--- src/main/java/org/dicemc/diceapi/obj/WorldEditObject.java
+++ src/main/java/org/dicemc/diceapi/obj/WorldEditObject.java
@@ -1,35 +1,33 @@
 package org.dicemc.diceapi.obj;
 
-import com.boydti.fawe.util.EditSessionBuilder;
 import com.sk89q.worldedit.*;
-import com.sk89q.worldedit.bukkit.BukkitCommandSender;
-import com.sk89q.worldedit.bukkit.BukkitPlayer;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.bukkit.BukkitWorld;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
-import com.sk89q.worldedit.extension.input.ParserContext;
 import com.sk89q.worldedit.extension.platform.Actor;
 import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
-import com.sk89q.worldedit.extent.clipboard.io.BuiltInClipboardFormat;
-import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
-import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
-import com.sk89q.worldedit.extent.clipboard.io.ClipboardWriter;
-import com.sk89q.worldedit.function.pattern.Pattern;
+import com.sk89q.worldedit.extent.clipboard.io.*;
+import com.sk89q.worldedit.function.operation.Operation;
+import com.sk89q.worldedit.function.operation.Operations;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.Region;
-import com.sk89q.worldedit.regions.RegionSelector;
-import com.sk89q.worldedit.world.block.BaseBlock;
-import org.bukkit.Bukkit;
+import com.sk89q.worldedit.session.ClipboardHolder;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 import org.dicemc.diceapi.DiceAPI;
 import javax.annotation.Nullable;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+import static org.bukkit.Bukkit.getLogger;
+import static org.bukkit.Bukkit.getWorld;
 
 public class WorldEditObject {
     private final WorldEditPlugin we;
@@ -63,29 +61,6 @@ public class WorldEditObject {
         return BlockVector3.at(location.getBlockX(), location.getBlockY(), location.getBlockZ());
     }
 
-    public void pasteSchematicAtVector(String world, String schematic, BlockVector3 location) throws IOException {
-        File schematicFile = new File(schematic);
-        BukkitWorld localWorld = getLocalWorld(world);
-        if (localWorld == null) {
-            // We got an issue here!
-            return;
-        }
-        ClipboardFormat clipboardFormat = ClipboardFormats.findByFile(schematicFile);
-        if (clipboardFormat == null) {
-            // We got an issue here!
-            return;
-        }
-        clipboardFormat.load(schematicFile).paste(localWorld, location, true, false, null);
-    }
-
-    public void pasteSchematic(String world, String schematic, Location location) {
-        try {
-            pasteSchematicAtVector(world, schematic, toVector(location));
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
     public void savePlayerClipboardAsSchematic(Player player, File file) {
         LocalSession session = we.getSession(player);
         try (ClipboardWriter writer = BuiltInClipboardFormat.SPONGE_SCHEMATIC.getWriter(new FileOutputStream((file)))) {
@@ -114,20 +89,45 @@ public class WorldEditObject {
         }
     }
 
-    public BaseBlock getBlock(String id) throws WorldEditException {
-        return we.getWorldEdit().getBlockFactory().parseFromInput(id, new ParserContext());
+    public void pasteSchematicAtVector(World world, String schematic, Vector location) {
+        BlockVector3 vector3 = BlockVector3.at(location.getX(), location.getY(), location.getZ());
+        File schema = new File(schematic);
+        getLogger().info(schematic);
+
+        ClipboardFormat format = ClipboardFormats.findByFile(schema);
+
+        ClipboardReader reader = null;
+        try {
+            reader = format.getReader(new FileInputStream(schema));
+        } catch (IOException e) {
+            e.printStackTrace();
+        };
+
+        try {
+            Clipboard clipboard = reader.read();
+
+            com.sk89q.worldedit.world.World adaptedWorld = getLocalWorld(world);
+
+            EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(adaptedWorld,-1);
+
+            // Saves our operation and builds the paste - ready to be completed.
+            Operation operation = new ClipboardHolder(clipboard).createPaste(editSession)
+                    .to(vector3).ignoreAirBlocks(true).build();
+
+            try { // This simply completes our paste and then cleans up.
+                Operations.complete(operation);
+                editSession.flushSession();
+
+            } catch (WorldEditException e) { // If worldedit generated an exception it will go here
+                getLogger().severe("OOPS! Something went wrong, please contact an administrator");
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public int setAreaWithBlock(String world, Location minPoint, Location maxPoint, String blockId) {
-        LocalSession session = getLocalSession();
-        BukkitWorld bukkitWorld = getLocalWorld(world);
-        if (bukkitWorld == null) {
-            return 0;
-        }
-        RegionSelector selector = session.getRegionSelector(bukkitWorld);
-        selector.selectPrimary(toVector(minPoint), null);
-        selector.selectSecondary(toVector(maxPoint), null);
-        EditSession editSession = new EditSession(new EditSessionBuilder(bukkitWorld));
-        return editSession.setBlocks(selector.getRegion(), getBlock(blockId));
+    public void pasteSchematic(World world, String schematic, Vector location) {
+        pasteSchematicAtVector(world, schematic, location);
     }
 }
